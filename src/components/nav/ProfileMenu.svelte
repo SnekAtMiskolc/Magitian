4ff7c49add9ef646eba2c9c03d9d1a8a2ec264ca
Susa Milán <mailtosnek@protonmail.com>
2023-01-28 15:01:18 +0100

    Profile menu

---
+++ src/components/nav/ProfileMenu.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+<div class="absolute right-0 z-10 mt-2 w-auto whitespace-nowrap origin-top-right border-spcx-400 border-2 rounded-md bg-spcx-700  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+    <!-- Active: "bg-gray-100", Not Active: "" -->
+    <span class="font-bold text-2xl sm:text-xl block px-4 py-2 text-white " role="menuitem" tabindex="-1" id="user-menu-item-0">👋 Hi <span class="text-blue-500">Jerry Smith</span>!</span>
+    <hr class="border-spcx-400 my-2">
+    <a href="/" class="block px-4 text-xl sm:text-lg py-2 hover:bg-blue-600  text-white " role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+    <a href="/" class="block px-4 text-xl sm:text-lg py-2 hover:bg-blue-600  text-white " role="menuitem" tabindex="-1" id="user-menu-item-0">Your Repositories</a>
+    <a href="/" class="block px-4 text-xl sm:text-lg py-2 hover:bg-blue-600  text-white " role="menuitem" tabindex="-1" id="user-menu-item-0">Your Organizations</a>
+    <a href="/" class="block px-4 text-xl sm:text-lg py-2 hover:bg-blue-600  text-white " role="menuitem" tabindex="-1" id="user-menu-item-0">Your Supporters</a>
+    <hr class="border-spcx-400 my-2">
+    <a href="/" class="block px-4 py-2 text-xl sm:text-lg hover:bg-blue-600  text-white" role="menuitem" tabindex="-1" id="user-menu-item-1">Deployment</a>
+    <a href="/" class="block px-4 py-2 text-xl sm:text-lg hover:bg-blue-600  text-white" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+    <hr class="border-spcx-400 my-2">
+    <a href="/" class="font-bold block px-4 py-2 text-xl sm:text-lg hover:bg-mythic-400 hover:text-spcx-700  text-mythic-400" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+</div>
