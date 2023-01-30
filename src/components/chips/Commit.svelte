8bc2b1ec49be5682c213902921959605b11893c0
Susa Milán <mailtosnek@protonmail.com>
2023-01-30 18:17:33 +0100

    Added commit chip

---
+++ src/components/chips/Commit.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let commit_id: string
+</script>
+
+<span><a class="text-blue-600 underline" href="/">{commit_id}</a> Fixed indexing bug in the Insert main function </span>
