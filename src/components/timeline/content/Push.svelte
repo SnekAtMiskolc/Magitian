0fb08af0577311957220b322f3aca86898fa25dd
Susa Milán <mailtosnek@protonmail.com>
2023-01-30 18:18:16 +0100

    added push timeline card content

---
+++ src/components/timeline/content/Push.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Commit from "../../chips/Commit.svelte";
+</script>
+
+<div class="text-white">
+    <h1 class="font-bold text-xl">
+        <a href="/LinusTorvalds" class="text-blue-500">Linus Torvalds</a> pushed to <a href="/LinusTorvalds/linux" class="text-mythic-400">linux</a>
+    </h1>
+    <hr class="border-spcx-400 my-2">
+    <div>
+        <h2 class="font-bold">2 Commits to master</h2>
+        <div class="grid mt-2">
+            <Commit commit_id="12del5g"></Commit>
+            <Commit commit_id="7fjk342"></Commit>
+        </div>
+    </div>
+</div>
