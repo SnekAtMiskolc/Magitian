b16dc9777b647e4148a79b3666115dcfe8285814
Susa Milán <mailtosnek@protonmail.com>
2023-01-30 18:17:44 +0100

    Added repo chip

---
+++ src/components/chips/RepoChip.svelte
@@ -0,0 +1,5 @@
+
+<span class="text-white flex">
+    <img class="rounded-full w-5 h-5 mr-1 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+    <a class="font-medium underline text-mythic-400" href="/LinusTorvalds/linux">LinusTorvalds/linux</a>
+</span>
