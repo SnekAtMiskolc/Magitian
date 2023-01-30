46946a2b30472dbc92384acb3d1f7d9f39434391
Susa Milán <mailtosnek@protonmail.com>
2023-01-30 18:18:46 +0100

    Tesing components on +page.svelte + layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import TimeLineCard from "../components/timeline/TimeLineCard.svelte";
+    import Push from "../components/timeline/content/Push.svelte";
+    import RepoChip from "../components/chips/RepoChip.svelte";
+</script>
+    
+<main>
+    <div id="recent" class="bg-spcx-700 p-4 m-4 rounded-xl border-2 border-spcx-400">
+        <h1 class="font-bold text-white">Recent activity</h1>
+        <hr class="border-spcx-400 my-2">
+        <div class="grid">
+            <RepoChip></RepoChip>
+            <RepoChip></RepoChip>
+            <RepoChip></RepoChip>
+        </div>
+    </div>
+    <div id="timeline" class="">
+        <TimeLineCard><Push></Push></TimeLineCard>
+        <TimeLineCard><Push></Push></TimeLineCard>
+        <TimeLineCard><Push></Push></TimeLineCard>
+    </div>
+</main>
