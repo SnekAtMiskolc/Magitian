001adfbb7a6169775b5f360e1266bcc0ed857ec2
Susa Milán <mailtosnek@protonmail.com>
2023-01-31 15:42:08 +0100

    Added profile picture to Push.svelte

--- src/components/timeline/content/Push.svelte
+++ src/components/timeline/content/Push.svelte
@@ -3,9 +3,12 @@
 </script>
 
 <div class="text-white">
-    <h1 class="font-bold text-xl">
-        <a href="/LinusTorvalds" class="text-blue-500">Linus Torvalds</a> pushed to <a href="/LinusTorvalds/linux" class="text-mythic-400">linux</a>
-    </h1>
+    <div class="flex place-items-center">
+        <img class="rounded-full w-10 h-10 mr-1 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+        <h1 class="font-bold text-xl">
+            <a href="/LinusTorvalds" class="text-blue-500">Linus Torvalds</a> pushed to <a href="/LinusTorvalds/linux" class="text-mythic-400">linux</a>
+        </h1>
+    </div>
     <hr class="border-spcx-400 my-2">
     <div>
         <h2 class="font-bold">2 Commits to master</h2>
