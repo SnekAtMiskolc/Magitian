a2ba6c3314a394f865f4312cc09864d0c8b37255
Susa Milán <mailtosnek@protonmail.com>
2023-01-31 15:41:51 +0100

    Update timelinecard component

--- src/components/timeline/TimeLineCard.svelte
+++ src/components/timeline/TimeLineCard.svelte
@@ -1,3 +1,7 @@
-<div class="bg-spcx-700 rounded-xl p-4 m-4 border-2 border-spcx-400 sm:w-2/3 xsm:p-4">
-    <slot><!-- optional fallback --></slot>
+
+<div class="m-4">
+    <h2 class="text-spcx-600 mt-2 mb-1 ml-4">2 months ago - 2022.11.01</h2>
+    <div class="bg-spcx-700 rounded-xl p-4 border-2 border-spcx-400 sm:w-2/3 xsm:p-4">
+        <slot><!-- optional fallback --></slot>
+    </div>
 </div>
