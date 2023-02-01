c23766e1679112698468cdf2dce9f983b997df75
Susa Milán <mailtosnek@protonmail.com>
2023-02-01 18:03:15 +0100

    Fixed the size of the TimeLineCard and the Recent activity field

--- src/components/timeline/TimeLineCard.svelte
+++ src/components/timeline/TimeLineCard.svelte
@@ -1,7 +1,7 @@
 
-<div class="m-4">
+<div class="m-4 sm:w-2/3">
     <h2 class="text-spcx-600 mt-2 mb-1 ml-4">2 months ago - 2022.11.01</h2>
-    <div class="bg-spcx-700 rounded-xl p-4 border-2 border-spcx-400 sm:w-2/3 xsm:p-4">
+    <div class="bg-spcx-700 rounded-xl p-4 border-2 border-spcx-400 xsm:p-4">
         <slot><!-- optional fallback --></slot>
     </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
     
 <main>
-    <div id="recent" class="bg-spcx-700 p-4 m-4 rounded-xl border-2 border-spcx-400">
+    <div id="recent" class="bg-spcx-700 p-4 m-4 rounded-xl border-2 border-spcx-400 sm:w-2/3">
         <h1 class="font-bold text-white text-2xl">Recent activity</h1>
         <hr class="border-spcx-400 my-2">
         <div class="grid">
