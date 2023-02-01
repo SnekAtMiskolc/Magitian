a80d1501091aca512e311d2b6e70a2a37c0567b4
Susa Milán <mailtosnek@protonmail.com>
2023-02-01 18:33:30 +0100

    Did some layout work on / :D

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,25 @@
     import TimeLineCard from "../components/timeline/TimeLineCard.svelte";
     import Push from "../components/timeline/content/Push.svelte";
     import RepoChip from "../components/chips/RepoChip.svelte";
+    import RecentActivity from "../components/timeline/RecentActivity.svelte";
 </script>
     
 <main>
-    <div id="recent" class="bg-spcx-700 p-4 m-4 rounded-xl border-2 border-spcx-400 sm:w-2/3">
-        <h1 class="font-bold text-white text-2xl">Recent activity</h1>
-        <hr class="border-spcx-400 my-2">
-        <div class="grid">
-            <RepoChip></RepoChip>
-            <RepoChip></RepoChip>
-            <RepoChip></RepoChip>
+    <div class="flex justify-center">
+        <div class="w-full sm:w-4/5 md:w-2/3 lg:w-2/4 xl:w-2/4">
+            <RecentActivity>
+                <RepoChip></RepoChip>
+                <RepoChip></RepoChip>
+                <RepoChip></RepoChip>
+            </RecentActivity>
+
+                <h1 class="m-2 text-teal-500 font-bold text-3xl text-center">Timeline</h1>
+                <hr class="border-spcx-400">
+
+            <div id="timeline" class="">
+                <TimeLineCard><Push></Push></TimeLineCard>
+            </div>
         </div>
-    </div>
-    <div id="timeline" class="">
-        <TimeLineCard><Push></Push></TimeLineCard>
+
     </div>
 </main>
