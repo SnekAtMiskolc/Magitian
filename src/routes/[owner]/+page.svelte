ab1b4e2d8c3a6e2cf777884229ccf21939e30918
Susa Milán <mailtosnek@protonmail.com>
2023-02-01 18:43:29 +0100

    Init layout for /owner

--- src/routes/[owner]/+page.svelte
+++ src/routes/[owner]/+page.svelte
@@ -1,3 +1,20 @@
-<script lang="">
+<script lang="ts">
 
 </script>
+
+<main>
+    <!-- Sidebar / profile view -->
+    <div>
+        <!-- Profile picture -->
+        <div>
+
+        </div>
+        <!-- Profile stats -->
+        <div>
+
+        </div>
+    </div>
+    <div>
+
+    </div>
+</main>
