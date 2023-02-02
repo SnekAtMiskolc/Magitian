3f06e653e5a4951cb6c8e10a4709a9e67d05e003
Susa Milán <mailtosnek@protonmail.com>
2023-02-02 16:44:36 +0100

    Added BIO and Status

--- src/routes/[owner]/+page.svelte
+++ src/routes/[owner]/+page.svelte
@@ -5,6 +5,22 @@
 
 <main>
     <SideBar></SideBar>
+    <div class="mx-4 mb-4">
+        <div class="grid space-y-3">
+            <div class="bg-spcx-700 rounded-xl p-2 text-white font-semibold flex flex-row">
+                <div class="border-r border-spcx-400 mr-2">
+                    <h3 class="mr-1">🐧</h3>
+                </div>
+                <h3>Doing some programming!</h3>
+            </div>
+            <div class="text-white font-semibold bg-spcx-700 p-2 rounded-xl">
+                <h3 class="text-spcx-400">Bio</h3>
+            
+                <hr class="border-spcx-400 my-1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis aut magni qui cum est eveniet officia porro ipsa? Iusto ab, aspernatur aperiam unde quos nam?</p>
+            </div>
+        </div>
+    </div>
 
     <hr class="border-spcx-400">
 
