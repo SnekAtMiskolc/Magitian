57826a07c1f272fd7fbe8c569bcac489e6b8d4ee
Susa Milán <mailtosnek@protonmail.com>
2023-02-02 16:11:31 +0100

    edit placement

--- src/components/timeline/RecentActivity.svelte
+++ src/components/timeline/RecentActivity.svelte
@@ -21,7 +21,7 @@
 <div id="recent" class="bg-spcx-700 m-4 rounded-xl border-2 border-spcx-400 ">
     <!-- Idea: Clicking the title will minimize -->
     <!-- Idea: Clicking the title will minimize on mobile and on desktop there will be a button -->
-    <div class="flex justify-between justify-items-center hover:bg-spcx-600 p-4 rounded-t-xl" on:click={toggleDown} on:keydown>
+    <div class="flex justify-between justify-items-center hover:bg-spcx-600 p-4 rounded-t-xl cursor-pointer" on:click={toggleDown} on:keydown>
         <h1 class="font-bold text-white text-xl">Recent activity</h1>
     <!-- Idea: This button will minimize -->
     {#if down}
@@ -31,7 +31,7 @@
     {/if}
     </div>
     <hr class="border-spcx-400 mb-2">
-    <div class="grid {state}">
+    <div class="grid px-4 pb-2 {state}">
         <slot><h2 class="text-white font-bold text-lg">Nothing but crickets...</h2></slot>
     </div>
 </div>
