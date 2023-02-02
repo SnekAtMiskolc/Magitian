855c130682d57cb5d232538561de2a995c4029a5
Susa Milán <mailtosnek@protonmail.com>
2023-02-02 16:11:01 +0100

    Redo owner layout

--- src/routes/[owner]/+page.svelte
+++ src/routes/[owner]/+page.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
+    import SideBar from '../../components/owner/SideBar.svelte';
 
 </script>
 
 <main>
-    <!-- Sidebar / profile view -->
+    <SideBar></SideBar>
+
+    <hr class="border-spcx-400">
+
     <div>
-        <!-- Profile picture -->
-        <div>
 
-        </div>
-        <!-- Profile stats -->
-        <div>
-
-        </div>
     </div>
-    <div>
+    <!-- Profile stats -->
+    <div class="w-1/2 pt-8 p-4">
 
     </div>
 </main>
