4b377635a059cea06ffd695115afa4711f62b125
Susa Milán <mailtosnek@protonmail.com>
2023-02-02 11:16:36 +0100

    Make margins better

--- src/components/timeline/TimeLineCard.svelte
+++ src/components/timeline/TimeLineCard.svelte
@@ -1,6 +1,6 @@
 
-<div class="m-4 ">
-    <h2 class="text-spcx-600 mt-2 mb-1 ml-4">2 months ago - 2022.11.01</h2>
+<div class="m-4">
+    <h2 class="text-spcx-600 my-4">2 months ago - 2022.11.01</h2>
     <div class="bg-spcx-700 rounded-xl p-4 border-2 border-spcx-400 xsm:p-4">
         <slot><!-- optional fallback --></slot>
     </div>
