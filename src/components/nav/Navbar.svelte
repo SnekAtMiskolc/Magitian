fdaf963774b8a856e151d98615582f33b89a0490
Susa Milán <mailtosnek@protonmail.com>
2023-02-02 16:20:56 +0100

    Cleanup the funni mobile menu button

--- src/components/nav/Navbar.svelte
+++ src/components/nav/Navbar.svelte
@@ -18,7 +18,7 @@
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
-          <button on:click={() => {mobileOn = !mobileOn}} type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <button on:click={() => {mobileOn = !mobileOn}} type="button" class="inline-flex items-center justify-center rounded-md p-2 text-white hover:text-zinc-500 focus:outline-none " aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <!--
               Icon when menu is closed.
