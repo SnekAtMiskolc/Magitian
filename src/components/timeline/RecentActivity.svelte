579266d8424b4c30e62a1ea595978c29d6480ee1
Susa Milán <mailtosnek@protonmail.com>
2023-02-02 11:16:23 +0100

    Add dropdown

--- src/components/timeline/RecentActivity.svelte
+++ src/components/timeline/RecentActivity.svelte
@@ -1,14 +1,37 @@
-    
-<div id="recent" class="bg-spcx-700 p-4 m-4 rounded-xl border-2 border-spcx-400 ">
+<script lang="ts">
+    import Icon from 'svelte-awesome/components/Icon.svelte'
+
+    import chevronUp from 'svelte-awesome/icons/chevronUp';
+    import chevronDown from 'svelte-awesome/icons/chevronDown';
+
+    let down = true
+    let state: string = 'block'
+
+    $: if(down) {
+        state = 'block'
+    } else {
+        state = 'hidden'
+    }
+
+    const toggleDown = () => {
+        down = !down
+    }
+</script>
+
+<div id="recent" class="bg-spcx-700 m-4 rounded-xl border-2 border-spcx-400 ">
     <!-- Idea: Clicking the title will minimize -->
     <!-- Idea: Clicking the title will minimize on mobile and on desktop there will be a button -->
-    <div class="flex justify-between justify-items-center" on:click={() => {window.alert("asd")}}>
-        <h1 class="font-bold text-white text-2xl">Recent activity</h1>
+    <div class="flex justify-between justify-items-center hover:bg-spcx-600 p-4 rounded-t-xl" on:click={toggleDown} on:keydown>
+        <h1 class="font-bold text-white text-xl">Recent activity</h1>
     <!-- Idea: This button will minimize -->
-        <button class="font-bold hover:bg-spcx-600 text-white p-2 rounded-xl">Minimize</button>
+    {#if down}
+        <Icon data={chevronUp} style="color: #FFFFFF"></Icon>
+    {:else}
+        <Icon data={chevronDown} style="color: #FFFFFF"></Icon>
+    {/if}
     </div>
-    <hr class="border-spcx-400 my-2">
-    <div class="grid">
+    <hr class="border-spcx-400 mb-2">
+    <div class="grid {state}">
+        <slot><h2 class="text-white font-bold text-lg">Nothing but crickets...</h2></slot>
     </div>
-    <slot><h2 class="text-white font-bold text-xl">Nothing but crickets...</h2></slot>
 </div>
