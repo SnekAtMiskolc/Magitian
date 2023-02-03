96481b543fb53fb146edb045fa91cca36731a2b3
Susa Milán <mailtosnek@protonmail.com>
2023-02-03 21:08:20 +0100

    Added Button component

---
+++ src/components/general/Button.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let onClick = () => {console.log('Clicked')}
+</script>
+
+<button on:click={onClick} class="p-2 bg-blue-600 w-28 h-10 hover:bg-blue-700 active:bg-blue-600 w-28 rounded-xl"><slot>Button</slot></button>
