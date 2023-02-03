a71db2304f9f1530dec6df0ae7617b46c38c628e
Susa Milán <mailtosnek@protonmail.com>
2023-02-03 21:12:28 +0100

    Did some reisizing on the profile view

--- src/components/owner/SideBar.svelte
+++ src/components/owner/SideBar.svelte
@@ -1,9 +1,12 @@
+<script lang="ts">
+    import Button from "../general/Button.svelte";
+</script>
 
-<div class="flex justify-between mr-4">
+<div class="flex justify-between my-4">
     <!-- Sidebar / profile view -->
     <div class="flex">
         <!-- Profile picture -->
-        <img class="p-4 rounded-full w-32 mr-1 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+        <img class="rounded-full w-24 mr-2 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
         <div class="flex items-center">
             <div class="flex flex-col">
                 <h1 class="text-white text-2xl font-bold">Linus Torvalds</h1>
@@ -13,6 +16,6 @@
     </div>
 
     <div class="rounded-xl  text-white font-semibold flex justify-center items-center">
-        <button class="p-2 bg-blue-600 w-28 h-10 hover:bg-blue-700 active:bg-blue-600 w-28 rounded-xl">Follow</button>
+        <Button>Follow</Button>
     </div>
 </div>
