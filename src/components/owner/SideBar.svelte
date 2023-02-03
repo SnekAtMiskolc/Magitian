65b0df328c840b8414641f9ea7eba44916d50c1e
Susa Milán <mailtosnek@protonmail.com>
2023-02-03 20:46:04 +0100

    Follow button in sidebar

--- src/components/owner/SideBar.svelte
+++ src/components/owner/SideBar.svelte
@@ -1,12 +1,18 @@
 
-<!-- Sidebar / profile view -->
-<div class="flex">
-    <!-- Profile picture -->
-    <img class="p-4 rounded-full w-32 mr-1 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-    <div class="flex items-center">
-        <div class="flex flex-col">
-            <h1 class="text-white text-2xl font-bold">Linus Torvalds</h1>
-            <h2 class="text-spcx-600 font-semibold">LinusTorvalds</h2>
+<div class="flex justify-between mr-4">
+    <!-- Sidebar / profile view -->
+    <div class="flex">
+        <!-- Profile picture -->
+        <img class="p-4 rounded-full w-32 mr-1 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+        <div class="flex items-center">
+            <div class="flex flex-col">
+                <h1 class="text-white text-2xl font-bold">Linus Torvalds</h1>
+                <h2 class="text-spcx-600 font-semibold">LinusTorvalds</h2>
+            </div>
         </div>
+    </div>
+
+    <div class="rounded-xl  text-white font-semibold flex justify-center items-center">
+        <button class="p-2 bg-blue-600 w-28 h-10 hover:bg-blue-700 active:bg-blue-600 w-28 rounded-xl">Follow</button>
     </div>
 </div>
