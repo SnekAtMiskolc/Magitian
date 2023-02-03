940adae48869c13aefc39ecc0fe01b05309c301f
Susa Milán <mailtosnek@protonmail.com>
2023-02-03 20:46:29 +0100

    Moved follow tab

--- src/routes/[owner]/+page.svelte
+++ src/routes/[owner]/+page.svelte
@@ -3,6 +3,7 @@
 
     import Icon from 'svelte-awesome/components/Icon.svelte'
 
+    // Icons
     import globe from 'svelte-awesome/icons/globe';
     import twitter from 'svelte-awesome/icons/twitter';
     import linkedinSquare from 'svelte-awesome/icons/linkedinSquare';
@@ -10,21 +11,13 @@
     import book from 'svelte-awesome/icons/book';
     import thList from 'svelte-awesome/icons/thList';
     import bookmark from 'svelte-awesome/icons/bookmark';``
-
-    import chevronUp from 'svelte-awesome/icons/chevronUp';
-    import chevronDown from 'svelte-awesome/icons/chevronDown';
-
 </script>
 
 <main>
+    <div class="flex md:flex-row">
+    <div class="mx-4 mb-4 sm:w-sidebar sm:mx-auto">
     <SideBar></SideBar>
-    <div class="mx-4 mb-4">
         <div class="grid space-y-3">
-            <div class="bg-spcx-700 rounded-xl  text-white font-semibold flex">
-                <h1 class="p-2 bg-mythic-400 rounded-l-xl w-26 text-center">21 Follows</h1>
-                <h1 class="p-2 bg-green-600 border-spcx-900 border-l-4 flex-grow text-center">124000 Followers</h1>
-                <button class="border-l-4 border-spcx-900 p-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-600 w-28 rounded-r-xl">Follow</button>
-            </div>
             <div class="bg-spcx-700 rounded-xl p-2 text-white font-semibold flex flex-row">
                 <div class="border-r border-spcx-400 mr-2">
                     <h3 class="mr-1">🐧</h3>
@@ -41,27 +34,29 @@
             <!-- If the user doesn't have one of them then we shall lower it's opacity see: twitter -->
             <div class="text-white font-semibold bg-spcx-700 p-2 rounded-full flex justify-between">
                 <a href="/">
-                    <div class="bg-blue-600 p-2 rounded-full">
+                    <div class="bg-blue-600 p-2 rounded-full w-full">
                         <Icon data={globe} scale={1.52}></Icon><span class="ml-1">site</span>
                     </div>
                 </a>
                 <a href="/">
-                    <div class="bg-blue-500 p-2 rounded-full opacity-20 cursor-not-allowed">
+                    <div class="bg-blue-500 p-2 rounded-full opacity-20 cursor-not-allowed w-full">
                         <Icon data={twitter} scale={1.52}></Icon><span class="ml-1">twitter</span>
                     </div>
                 </a>
                 <a href="/">
-                    <div class="bg-blue-800 p-2 rounded-full cursor-pointer">
+                    <div class="bg-blue-800 p-2 rounded-full cursor-pointer w-full">
                         <Icon data={linkedinSquare} scale={1.52}></Icon><span class="ml-1">linkedin</span>
                     </div>
                 </a>
-                <a href="/">
-                    <div class="bg-mythic-500 p-2 rounded-full">
+                <a href="/" class="">
+                    <div class="bg-mythic-500 p-2 rounded-full w-full">
                         <Icon data={youtubePlay} scale={1.52}></Icon><span class="ml-1">YouTube</span>
                     </div>
                 </a>
             </div>
         </div>
+    </div>
+
     </div>
 
 
