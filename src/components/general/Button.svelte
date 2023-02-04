9028c91162bf924ed68b5aae7596bd468af9c13a
Susa Milán <mailtosnek@protonmail.com>
2023-02-04 10:44:55 +0100

    Sizing for Sidebar

--- src/components/general/Button.svelte
+++ src/components/general/Button.svelte
@@ -2,4 +2,4 @@
     export let onClick = () => {console.log('Clicked')}
 </script>
 
-<button on:click={onClick} class="p-2 bg-blue-600 w-28 h-10 hover:bg-blue-700 active:bg-blue-600 w-28 rounded-xl"><slot>Button</slot></button>
+<button on:click={onClick} class="p-2 w-28 h-10 w-28 rounded-xl bg-gradient-to-r from-mythic-500 to-blue-600"><slot>Button</slot></button>
--- src/components/owner/SideBar.svelte
+++ src/components/owner/SideBar.svelte
@@ -2,11 +2,11 @@
     import Button from "../general/Button.svelte";
 </script>
 
-<div class="flex justify-between my-4">
+<div class="flex justify-between my-4 md:flex-col">
     <!-- Sidebar / profile view -->
-    <div class="flex">
+    <div class="flex md:flex-col md:justify-center md:items-center md:mb-4">
         <!-- Profile picture -->
-        <img class="rounded-full w-24 mr-2 bg-center" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+        <img class="rounded-full w-24 mr-2 bg-center md:w-52" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
         <div class="flex items-center">
             <div class="flex flex-col">
                 <h1 class="text-white text-2xl font-bold">Linus Torvalds</h1>
@@ -15,7 +15,9 @@
         </div>
     </div>
 
-    <div class="rounded-xl  text-white font-semibold flex justify-center items-center">
+    <div class="rounded-xl  text-white font-semibold flex justify-center items-center md:jusify-between">
+        <span class="mr-3 ">42 Follows</span>
+        <span class="mr-3 ">12000 Followers</span>
         <Button>Follow</Button>
     </div>
 </div>
