64bdad9fcc1f339b6e1422e7e3d0b93514007aa7
Susa Milán <mailtosnek@protonmail.com>
2023-02-06 08:48:56 +0100

    Tab changes

--- src/components/general/TabHead.svelte
+++ src/components/general/TabHead.svelte
@@ -1,10 +1,11 @@
 <script>
   export let divClass = ''
-  export let ulClass = 'flex flex-wrap -mb-px'
+  export let ulClass = 'flex flex-row'
 </script>
 
 <div class={divClass}>
-  <ul class={ulClass}  role="tablist">
-    <slot />
-  </ul>
+    <div class="mx-4">
+        <slot />
+    </div>
+    <hr class="border-spcx-400 my-4">
 </div>
