85495f6b78b85c1ad573f79f996347a4b6121765
Susa Milán <mailtosnek@protonmail.com>
2023-02-06 07:37:19 +0100

    Change labels and colors

--- src/components/nav/MobileMenu.svelte
+++ src/components/nav/MobileMenu.svelte
@@ -8,12 +8,12 @@
         <input type="text" name="" id="" class="bg-spcx-800 p-2 mb-2 text-lg w-full rounded text-white font-semibold" placeholder="Search for something...">
         <hr class="border-spcx-400 py-1">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="/" class="bg-spcx-900 text-lg text-white block px-3 py-2 rounded-md font-bold " aria-current="page">Dashboard</a>
+        <a href="/" class="bg-spcx-800 text-md text-white block px-3 py-2 rounded-md font-bold " aria-current="page">Home</a>
   
-        <a href="/" class="text-gray-300 text-lg hover:bg-spcx-800 hover:text-white block px-3 py-2 rounded-md font-bold">Team</a>
+        <a href="/" class="text-gray-300 text-md hover:bg-spcx-600 hover:text-white block px-3 py-2 rounded-md font-bold">Pull Requests</a>
   
-        <a href="/" class="text-gray-300 text-lg hover:bg-spcx-800 hover:text-white block px-3 py-2 rounded-md font-bold">Projects</a>
+        <a href="/" class="text-gray-300 text-md hover:bg-spcx-600 hover:text-white block px-3 py-2 rounded-md font-bold">Issues</a>
   
-        <a href="/" class="text-gray-300 text-lg hover:bg-spcx-800 hover:text-white block px-3 py-2 rounded-md font-bold">Calendar</a>
+        <a href="/" class="text-gray-300 text-md hover:bg-spcx-600 hover:text-white block px-3 py-2 rounded-md font-bold">Discover</a>
     </div>
 </div>
