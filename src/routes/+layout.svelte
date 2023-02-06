e4692f85cb1188889f4ae58a75ae7d28fbfb9598
Susa Milán <mailtosnek@protonmail.com>
2023-02-06 16:54:13 +0100

    Added footer to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,12 @@
     import '../app.css'
 
     import Navbar from '../components/nav/Navbar.svelte';
+
+    import Footer from '../components/nav/Footer.svelte';
 </script>
 
 <Navbar></Navbar>
 
 <slot></slot>
+
+<Footer></Footer>
