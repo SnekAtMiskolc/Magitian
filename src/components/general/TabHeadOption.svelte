173af5228bc4834bbe403df3fd50e1a1adc00c4f
Susa Milán <mailtosnek@protonmail.com>
2023-02-06 08:21:41 +0100

    TabHead

---
+++ src/components/general/TabHeadOption.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let id: number;
+  export let buttonClass = ''
+  export let liClass = 'mr-2'
+</script>
+
+<li class={liClass} role="presentation">
+  <button
+    on:click
+    class={buttonClass}
+    id="{id}-tabhead"
+    type="button"
+    role="tab">
+    <slot />
+  </button>
+</li>
