182bfad21bde2688e95c16ca8eb16c1f09d7d41e
Susa Milán <mailtosnek@protonmail.com>
2023-02-07 18:46:46 +0100

    change button color

--- src/components/general/Button.svelte
+++ src/components/general/Button.svelte
@@ -2,4 +2,4 @@
     export let onClick = () => {console.log('Clicked')}
 </script>
 
-<button on:click={onClick} class="p-2 w-28 h-10 w-28 rounded-xl bg-gradient-to-r from-mythic-500 to-blue-600 text-sm font-bold"><slot>Button</slot></button>
+<button on:click={onClick} class="p-2 w-28 h-10 w-28 rounded-xl bg-blue-600 text-sm font-bold"><slot>Button</slot></button>
