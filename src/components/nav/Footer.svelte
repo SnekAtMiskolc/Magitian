7f8ee91088fe90b29da2963fea7f7cc5527b6e38
Susa Milán <mailtosnek@protonmail.com>
2023-02-07 18:46:54 +0100

    fotter stuff

--- src/components/nav/Footer.svelte
+++ src/components/nav/Footer.svelte
@@ -1,4 +1,7 @@
-<div class="bg-spcx-800 h-48 p-4">
-    <h1 class="text-white font-bold text-4xl">Footer</h1>
-
+<div class="bg-spcx-700 h-48 p-4 bg-gradient-to-r from-blue-600 to-red-600 via-red-500">
+    <h1 class="text-spcx-900 font-bold text-4xl text-center">Magitian</h1>
+    <div class="flex justify-center justify-items-center">
+        <div>
+        </div>
+    </div>
 </div>
