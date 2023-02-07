34b5ce5c975a2b5565cce7378e5dc749ea0fdbbf
Susa Milán <mailtosnek@protonmail.com>
2023-02-07 18:47:17 +0100

    about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { fade, slide } from 'svelte/transition';
+
+    let a_place_for_your_code = false;
+
+    const handleScroll = () => {
+        a_place_for_your_code = window.scrollY > 3;
+    };
+</script>
+<h1 class="text-8xl text-white">magitian</h1>
+{#if a_place_for_your_code}
+    <h1 in:fade>Animate on Scroll</h1>
+    <p in:slide>This text is sliding in on scroll!</p>
+{/if}
