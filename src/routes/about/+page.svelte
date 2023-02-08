6c59ce79e79ee1de1808d841c5329228a8c60022
Susa Milán <mailtosnek@protonmail.com>
2023-02-08 12:07:11 +0100

    Extra card

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,28 +8,37 @@
 
         window.addEventListener("scroll", () => {
             scrollPosition = window.scrollY;
-            console.log(scrollPosition)
         });
     }
 
-    let divBase = "opacity-40"
+    let divBase = "opacity-0"
     let style = divBase;
+    let style2 = divBase;
     let divClass = "opacity-100";
 
     $: if(scrollPosition > 100) {
         style = divClass
-        console.log(style)
     } else {
         style = divBase
     }
+    $: if(scrollPosition > 275) {
+        style2 = divClass
+        console.log(style)
+    } else {
+        style2 = divBase
+    }
 </script>
 
-<h1 class="text-8xl text-white">magitian</h1>
-<h1 class="text-8xl text-white">magitian</h1>
+<h1 class="text-7xl text-white text-center font-bold">magitian</h1>
 
 <hr class="my-4 border-spcx-400">
 
 <div class="{style} h-sidebar transform-all duration-500 bg-blue-600 m-4 rounded-3xl p-4 text-white font-semibold">
+    <h1 class="text-white text-4xl font-bold">Magical code</h1>
+    <hr class="my-2">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed consequuntur earum quos possimus officiis, quae nulla suscipit voluptate! Quo, maiores deleniti id officia praesentium at impedit vitae dolorem accusamus ipsam?</p>
+</div>
+<div class="{style2} h-sidebar transform-all duration-500 bg-blue-600 m-4 rounded-3xl p-4 text-white font-semibold">
     <h1 class="text-white text-4xl font-bold">Magical code</h1>
     <hr class="my-2">
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed consequuntur earum quos possimus officiis, quae nulla suscipit voluptate! Quo, maiores deleniti id officia praesentium at impedit vitae dolorem accusamus ipsam?</p>
