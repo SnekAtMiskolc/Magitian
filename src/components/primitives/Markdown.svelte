83ff3cf8a97c59b0e58c0e8c23704b0bf1b64c3c
Susa Milán <mailtosnek@protonmail.com>
2023-02-10 15:23:23 +0100

    Create repo page and Markdown component page

---
+++ src/components/primitives/Markdown.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<div class="m-4 p-4 bg-spcx-700 border border-spcx-400">
+
+</div>
---
+++ src/routes/[owner]/[repo]/+page.svelte
@@ -0,0 +1,21 @@
+
+
+<main>
+    <!-- Repo info tab -->
+    <div class="bg-spcx-700 h-52 m-4 rounded-xl border-spcx-400 border p-4 text-white">
+        <h1 class="font-light text-lg"><a href="./" class="text-blue-500 hover:underline">LinusTorvalds</a><span class="mx-1">/</span><a href="./Linux" class="text-blue-500 font-semibold hover:underline">Linux</a></h1>
+        <hr class="border-spcx-400 my-2">
+        <div class="flex">
+
+        </div>
+    </div>
+    <!-- Code section -->
+    <div class="bg-spcx-700 m-4 p-4 rounded-xl border-spcx-400 border text-white">
+        <div>
+            <a class="font-bold" href="./">Linus Torvalds</a>
+            <div>
+                Merged dev-x86 into master
+            </div>
+        </div>
+    </div>
+</main>
