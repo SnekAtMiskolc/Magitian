4fa3c2b04b4412f133bae5e9543c8f9105b44ed7
Susa Milán <mailtosnek@protonmail.com>
2023-02-13 17:17:49 +0100

    Blob component

---
+++ src/components/repo/Blob.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Icon from 'svelte-awesome/components/Icon.svelte'
+    import fileTextO from 'svelte-awesome/icons/fileTextO';
+</script>
+
+<div class="bg-spcx-600 p-2 rounded-xl hover:bg-spcx-400">
+    <a href="./linux/">
+        <Icon data={fileTextO}></Icon>
+        test.c
+    </a>
+</div>
