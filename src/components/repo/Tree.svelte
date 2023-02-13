e570c7ea7a49a43d89ef59888f0669f58447b7ee
Susa Milán <mailtosnek@protonmail.com>
2023-02-13 17:17:53 +0100

    Tree component

---
+++ src/components/repo/Tree.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import folder from 'svelte-awesome/icons/folder';
+    import Icon from 'svelte-awesome/components/Icon.svelte'
+</script>
+
+<div class="bg-spcx-600 p-2 rounded-xl hover:bg-spcx-400">
+    <a href="./linux/">
+        <Icon data={folder}></Icon>
+        test
+    </a>
+</div>
